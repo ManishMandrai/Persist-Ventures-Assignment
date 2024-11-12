112524a409a88599f7567eaf62792efdb64b9278
ManishMandrai <manishkumarmandrai@gmail.com>
2024-11-12 21:34:05 +0530

    .

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -3,7 +3,7 @@ import React from "react";
 function Leaderboard() {
   const mockLeaderboardData = [
     { id: 1, name: "Jhon", connections: 15 },
-    { id: 2, name: "Bob", connections: 12 },
+    { id: 2, name: "Manish", connections: 12 },
     { id: 3, name: "Charlie", connections: 10 },
   ];
 
